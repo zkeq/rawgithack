593e72caac93a4dd768a87d18884112ba8e80c1e
Zkeq <admin@icodeq.com>
2023-02-08 11:35:15 +0800

    Replace URL : cdn.cmds.run

--- web/rawgithack.js
+++ web/rawgithack.js
@@ -2,39 +2,39 @@ const GITHUB_API_URL = 'https://api.github.com';
 
 const TEMPLATES = [
   [/^(https?):\/\/gitlab\.com\/([^\/]+.*\/[^\/]+)\/(?:raw|blob)\/(.+?)(?:\?.*)?$/i,
-   '$1://gl.githack.com/$2/raw/$3'],
+   '$1://gl.cmds.run/$2/raw/$3'],
   [/^(https?):\/\/bitbucket\.org\/([^\/]+\/[^\/]+)\/(?:raw|src)\/(.+?)(?:\?.*)?$/i,
-   '$1://bb.githack.com/$2/raw/$3'],
+   '$1://bb.cmds.run/$2/raw/$3'],
 
   // snippet file URL from web interface, with revision
   [/^(https?):\/\/bitbucket\.org\/snippets\/([^\/]+\/[^\/]+)\/revisions\/([^\/\#\?]+)(?:\?[^#]*)?(?:\#file-(.+?))$/i,
-   '$1://bb.githack.com/!api/2.0/snippets/$2/$3/files/$4'],
+   '$1://bb.cmds.run/!api/2.0/snippets/$2/$3/files/$4'],
   // snippet file URL from web interface, no revision
   [/^(https?):\/\/bitbucket\.org\/snippets\/([^\/]+\/[^\/\#\?]+)(?:\?[^#]*)?(?:\#file-(.+?))$/i,
-   '$1://bb.githack.com/!api/2.0/snippets/$2/HEAD/files/$3'],
+   '$1://bb.cmds.run/!api/2.0/snippets/$2/HEAD/files/$3'],
   // snippet file URLs from REST API
   [/^(https?):\/\/bitbucket\.org\/\!api\/2.0\/snippets\/([^\/]+\/[^\/]+\/[^\/]+)\/files\/(.+?)(?:\?.*)?$/i,
-   '$1://bb.githack.com/!api/2.0/snippets/$2/files/$3'],
+   '$1://bb.cmds.run/!api/2.0/snippets/$2/files/$3'],
   [/^(https?):\/\/api\.bitbucket\.org\/2.0\/snippets\/([^\/]+\/[^\/]+\/[^\/]+)\/files\/(.+?)(?:\?.*)?$/i,
-   '$1://bb.githack.com/!api/2.0/snippets/$2/files/$3'],
+   '$1://bb.cmds.run/!api/2.0/snippets/$2/files/$3'],
 
   // welcome rawgit refugees
   [/^(https?):\/\/(?:cdn\.)?rawgit\.com\/(.+?\/[0-9a-f]+\/raw\/(?:[0-9a-f]+\/)?.+)$/i,
-   '$1://gist.githack.com/$2'],
+   '$1://gist.cmds.run/$2'],
   [/^(https?):\/\/(?:cdn\.)?rawgit\.com\/([^\/]+\/[^\/]+\/[^\/]+|[0-9A-Za-z-]+\/[0-9a-f]+\/raw)\/(.+)/i,
-   '$1://raw.githack.com/$2/$3'],
+   '$1://raw.cmds.run/$2/$3'],
 
   [/^(https?):\/\/raw\.github(?:usercontent)?\.com\/([^\/]+\/[^\/]+\/[^\/]+|[0-9A-Za-z-]+\/[0-9a-f]+\/raw)\/(.+)/i,
-   '$1://raw.githack.com/$2/$3'],
+   '$1://raw.cmds.run/$2/$3'],
   [/^(https?):\/\/github\.com\/(.[^\/]+?)\/(.[^\/]+?)\/(?!releases\/)(?:(?:blob|raw)\/)?(.+?\/.+)/i,
-   '$1://raw.githack.com/$2/$3/$4'],
+   '$1://raw.cmds.run/$2/$3/$4'],
   [/^(https?):\/\/gist\.github(?:usercontent)?\.com\/(.+?\/[0-9a-f]+\/raw\/(?:[0-9a-f]+\/)?.+)$/i,
-   '$1://gist.githack.com/$2'],
+   '$1://gist.cmds.run/$2'],
 
   [/^(https?):\/\/git\.sr\.ht\/(~[^\/]+\/[^\/]+\/blob\/.+\/.+)/i,
-   '$1://srht.githack.com/$2'],
+   '$1://srht.cmds.run/$2'],
   [/^(https?):\/\/hg\.sr\.ht\/(~[^\/]+\/[^\/]+\/raw\/.+)/i,
-   '$1://srhgt.githack.com/$2']
+   '$1://srhgt.cmds.run/$2']
 ];
 
 function mergeSlashes(url) {
@@ -125,7 +125,7 @@ function show(element) {
 
     var ghUrl = maybeConvertUrl(url);
     if (ghUrl) {
-      var matches = ghUrl.match(/^(\w+:\/\/(raw).githack.com\/([^\/]+)\/([^\/]+))\/([^\/]+)\/(.*)/i);
+      var matches = ghUrl.match(/^(\w+:\/\/(raw).cmds.run\/([^\/]+)\/([^\/]+))\/([^\/]+)\/(.*)/i);
       if (!matches) {
         devEl.value = ghUrl;
         prodEl.value = cdnize(ghUrl);
